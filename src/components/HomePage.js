51b4f58be551863c35697c354eaf28a3a63135a4
Handai-Yamato <y,handai@crmlab.jp>
2022-12-14 19:46:21 +0900

    fix:framer motionのコードをリファクタリング

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,123 +1,98 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import icon from "../assets/img/icon.jpg";
 import Button from "./Button";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import { sm } from "./media";
 import { motion } from "framer-motion";
 
+const variants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      duration: 1.4,
+    },
+  },
+};
+
 const HomePage = () => {
   return (
-    <div>
-      <motion.div
-        initial={{ opacity: 0, y: 10 }} // 初期状態
-        animate={{ opacity: 1, y: 0 }} // マウント時
-        exit={{ opacity: 0, y: 10 }} // アンマウント時
-        transition={{
-          duration: 0.8,
-        }}
-      >
-        <HomeName>Yamato Handai</HomeName>
-        <p className="mt-1">(Coder / Basket man)</p>
-        <HomeIcon src={icon} alt="アイコン" />
-      </motion.div>
+    <motion.div variants={variants} initial="hidden" animate="visible">
+      <HomeName>Yamato Handai</HomeName>
+      <p className="mt-1">(Coder / Basket man)</p>
+      <HomeIcon src={icon} alt="アイコン" />
 
-      <motion.div
-        initial={{ opacity: 0, y: 10 }} // 初期状態
-        animate={{ opacity: 1, y: 0 }} // マウント時
-        exit={{ opacity: 0, y: 10 }} // アンマウント時
-        transition={{
-          duration: 0.8,
-        }}
-      >
+      <div>
         <h2>About</h2>
-        <p>
-          現在
-          <a href="https://crmlab.jp/" target={"blank"}>
-            株式会社CRMラボ
-          </a>
-          でWEBコーダーとして働いています。コーディング業務をはじめ、ECサイトの運営代行にも携わっており、楽天やShopifyをはじめとする様々なカートシステムを用いて、販促の企画や商品編集、デザインの修正など幅広く業務をしています。
-        </p>
-        <br></br>
-        <p>
-          元々、高校卒業後に農林水産省へ入り、土木技術を専門とした仕事に携わっていました。安定した給料で福利厚生も良かったのですが、仕事自体をあまり好きにはなれず、何か自分にあった職業はないか探す日々を送っていました。
-        </p>
-        <p>
-          WEBエンジニアという仕事を知って独学をはじめてから、これが天職かも！？と感じたため、転職し現在の会社に入社しました。
-        </p>
-        <br></br>
-        <p>
+      </div>
+      <p>
+        現在
+        <a href="https://crmlab.jp/" target={"blank"}>
+          株式会社CRMラボ
+        </a>
+        でWEBコーダーとして働いています。コーディング業務をはじめ、ECサイトの運営代行にも携わっており、楽天やShopifyをはじめとする様々なカートシステムを用いて、販促の企画や商品編集、デザインの修正など幅広く業務をしています。
+      </p>
+      <br></br>
+      <p>
+        元々、高校卒業後に農林水産省へ入り、土木技術を専門とした仕事に携わっていました。安定した給料で福利厚生も良かったのですが、仕事自体をあまり好きにはなれず、何か自分にあった職業はないか探す日々を送っていました。
+      </p>
+      <p>
+        WEBエンジニアという仕事を知って独学をはじめてから、これが天職かも！？と感じたため、転職し現在の会社に入社しました。
+      </p>
+      <br></br>
+      <p>
         現在はフロントエンドエンジニアとしてモダンな技術を用いて開発をしたい！と考えるようになり、Next.jsの勉強をしています。
-        </p>
-      </motion.div>
+      </p>
       <br></br>
 
-      <motion.div
-        initial={{ opacity: 0, y: 10 }} // 初期状態
-        animate={{ opacity: 1, y: 0 }} // マウント時
-        exit={{ opacity: 0, y: 10 }} // アンマウント時
-        transition={{
-          duration: 0.8,
-        }}
-      >
-        <div>
-          <h2>Bio</h2>
-          <p>1998年 宮崎県生まれ</p>
-          <p>2017年 宮崎県立都城農業高等学校卒業</p>
-          <p>2017年 農林水産省近畿農政局入省</p>
-          <p>2022年〜現在 株式会社CRMラボ</p>
-        </div>
-      </motion.div>
+      <div>
+        <h2>Bio</h2>
+        <p>1998年 宮崎県生まれ</p>
+        <p>2017年 宮崎県立都城農業高等学校卒業</p>
+        <p>2017年 農林水産省近畿農政局入省</p>
+        <p>2022年〜現在 株式会社CRMラボ</p>
+      </div>
 
-      <motion.div
-        initial={{ opacity: 0, y: 10 }} // 初期状態
-        animate={{ opacity: 1, y: 0 }} // マウント時
-        exit={{ opacity: 0, y: 10 }} // アンマウント時
-        transition={{
-          duration: 0.8,
-        }}
-      >
-        <div>
-          <h2>Pastime</h2>
-          <p>バスケ、<Link to="/uses">ガジェット集め</Link>、<a href="https://www.secure.instagram.com/">グルメ巡り</a>、ゲーム(fps)</p>
-        </div>
-      </motion.div>
+      <div>
+        <h2>Pastime</h2>
+        <p>
+          バスケ、<Link to="/uses">ガジェット集め</Link>、
+          <a href="https://www.secure.instagram.com/">グルメ巡り</a>、ゲーム(fps)
+        </p>
+      </div>
 
-      <motion.div
-        initial={{ opacity: 0, y: 10 }} // 初期状態
-        animate={{ opacity: 1, y: 0 }} // マウント時
-        exit={{ opacity: 0, y: 10 }} // アンマウント時
-        transition={{
-          duration: 0.8,
-        }}
-      >
-        <div>
-          <h2>Skills</h2>
-          <SkillsContent>
-            <SkillsTitle>
-              {"【"}Markup{"】"}
-            </SkillsTitle>
-            <SkillsText>HTML, CSS(Sass), Tailwind CSS</SkillsText>
-          </SkillsContent>
-          <SkillsContent>
-            <SkillsTitle>
-              {"【"}FrontEnd{"】"}
-            </SkillsTitle>
-            <SkillsText>JavaScript, gulp, React(a little...)</SkillsText>
-          </SkillsContent>
-          <SkillsContent>
-            <SkillsTitle>
-              {"【"}Others{"】"}
-            </SkillsTitle>
-            <SkillsText>git, npm</SkillsText>
-          </SkillsContent>
+      <div>
+        <h2>Skills</h2>
+        <SkillsContent>
+          <SkillsTitle>
+            {"【"}Markup{"】"}
+          </SkillsTitle>
+          <SkillsText>HTML, CSS(Sass), Tailwind CSS</SkillsText>
+        </SkillsContent>
+        <SkillsContent>
+          <SkillsTitle>
+            {"【"}FrontEnd{"】"}
+          </SkillsTitle>
+          <SkillsText>JavaScript, gulp, React(a little...)</SkillsText>
+        </SkillsContent>
+        <SkillsContent>
+          <SkillsTitle>
+            {"【"}Others{"】"}
+          </SkillsTitle>
+          <SkillsText>git, npm</SkillsText>
+        </SkillsContent>
 
-          <PortfolioLink to="/works">
-            <Button text={"My Portfolio >"} />
-          </PortfolioLink>
-        </div>
-      </motion.div>
-    </div>
+        <PortfolioLink to="/works">
+          <Button text={"My Portfolio >"} />
+        </PortfolioLink>
+      </div>
+    </motion.div>
   );
 };
 
--- src/components/_app.js
+++ src/components/_app.js
@@ -1,9 +1,8 @@
 import { AnimatePresence } from "framer-motion";
-
 import "styles/globals.css";
 
 const MyApp = ({ Component, pageProps, router }) => (
-  <AnimatePresence mode="wait">
+  <AnimatePresence mode="wait" initial={true} onExitComplete={() => window.scrollTo({ top: 0 })}>
     <Component key={router.asPath} {...pageProps} />
   </AnimatePresence>
 );
