471d55f7441336db78f3d4385a8e0f8c30d924c1
Handai-Yamato <y,handai@crmlab.jp>
2023-01-04 12:31:51 +0900

    fix 使用していないコードの削除

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./assets/styles/style.css";
 import App from "./App";
 import GlobalStyle from "./components/GlobalStyle";
 
